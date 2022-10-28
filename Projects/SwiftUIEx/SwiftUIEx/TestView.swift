a485b26199b656c2459943b769f2352dee4c7f01
KEEN <homurahomus@gmail.com>
2022-10-28 13:57:41 +0900

    make button highlight disable

--- Projects/SwiftUIEx/SwiftUIEx/TestView.swift
+++ Projects/SwiftUIEx/SwiftUIEx/TestView.swift
@@ -10,13 +10,15 @@ import SwiftUI
 struct TestView: View {
     var body: some View {
         VStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                .background(.green)
-            Image(systemName: "")
-                .background(.yellow)
-            Image(systemName: "star.fill")
-                .background(.red)
-            let _ = print(Image(systemName: ""), Image(systemName: "star.fill"))
+            Button {
+                print("clicked")
+            } label: {
+                Text("Button")
+                    .foregroundColor(.white)
+            }
+            .buttonStyle(StaticButtonStyle())
+            .background(.black)
+
         }
     }
 }
@@ -24,5 +26,11 @@ struct TestView: View {
 struct TestView_Previews: PreviewProvider {
     static var previews: some View {
         TestView()
+    }
+}
+
+struct StaticButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        return configuration.label
     }
 }
