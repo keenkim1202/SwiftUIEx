5555180da6b2f3d4bc21c85a0db6378119243d4a
KEEN <homurahomus@gmail.com>
2022-11-16 17:23:34 +0900

    add: ActivityViewEx test1

---
+++ Projects/ActivityViewEx/ActivityViewEx/ActivityViewExApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityViewExApp.swift
+//  ActivityViewEx
+//
+//  Created by KeenKim on 2022/11/16.
+//
+
+import SwiftUI
+
+@main
+struct ActivityViewExApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Projects/ActivityViewEx/ActivityViewEx/ContentView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  ActivityViewEx
+//
+//  Created by KeenKim on 2022/11/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var shareText: String = ""
+    @State var isPresentActivityView: Bool = false
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            TextField("write text you want to share", text: $shareText)
+                .background(.black.opacity(0.1))
+                .padding(20)
+            
+            Button {
+                self.isPresentActivityView.toggle()
+            } label: {
+                HStack {
+                    Image(systemName: "square.and.arrow.up")
+                        .imageScale(.large)
+                        .foregroundColor(.white)
+                    Text("Share")
+                        .foregroundColor(.white)
+                }
+                .padding(8)
+                .background(.blue)
+                .cornerRadius(8)
+            }
+            .sheet(isPresented: $isPresentActivityView, content: {
+                ActivityViewController(activityItems: [$shareText.wrappedValue])
+            })
+            .presentationDetents([.medium, .large])
+        }
+    }
+}
+
+struct ActivityViewController: UIViewControllerRepresentable {
+    var activityItems: [Any]
+    var applicationActivities: [UIActivity]? = nil
+    @Environment(\.presentationMode) var presentationMode
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ActivityViewController>) -> UIActivityViewController {
+        let controller = UIActivityViewController(activityItems: activityItems, applicationActivities: applicationActivities)
+        controller.completionWithItemsHandler = { activityType, completed, returnedItems, error in
+            self.presentationMode.wrappedValue.dismiss()
+        }
+        
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: UIActivityViewController, context: UIViewControllerRepresentableContext<ActivityViewController>) {}
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
