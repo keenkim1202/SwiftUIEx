eeef83f0a6165f9811f0f8fc67683f8400ba1862
KEEN <homurahomus@gmail.com>
2022-11-16 18:02:49 +0900

    fix: can show shareSheet on modally presented view

--- Projects/ActivityViewEx/ActivityViewEx/ContentView.swift
+++ Projects/ActivityViewEx/ActivityViewEx/ContentView.swift
@@ -8,39 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var shareText: String = ""
+    @State var isPresented: Bool = false
     
     var body: some View {
         VStack(alignment: .center) {
-            TextField("write text you want to share", text: $shareText)
-                .background(.black.opacity(0.1))
-                .padding(20)
-            
             Button {
-                shareAction(text: shareText)
+                self.isPresented = true
             } label: {
                 HStack {
-                    Image(systemName: "sUIRquare.and.arrow.up")
-                        .imageScale(.large)
-                        .foregroundColor(.white)
-                    Text("Share")
+                    Text("Show Button View")
                         .foregroundColor(.white)
                 }
                 .padding(8)
                 .background(.blue)
                 .cornerRadius(8)
             }
+            .sheet(isPresented: $isPresented) {
+                SheetView()
+            }
         }
     }
     
-    func shareAction(text: String) {
-        let activityVC = UIActivityViewController(activityItems: [text], applicationActivities: nil)
+    struct SheetView: View {
+        @State private var isShare = false
+        @State var shareText: String = ""
         
-        guard let firstScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-              let firstWindow = firstScene.windows.first,
-              let rootVC = firstWindow.rootViewController
-        else { return }
+        var body: some View {
+            VStack {
+                TextField("write text you want to share", text: $shareText)
+                    .background(.black.opacity(0.1))
+                    .padding(20)
+                
+                Button {
+                    isShare = true
+                } label: {
+                    HStack {
+                        Image(systemName: "square.and.arrow.up")
+                            .imageScale(.large)
+                            .foregroundColor(.white)
+                        Text("Share")
+                            .foregroundColor(.white)
+                    }
+                    .padding(8)
+                    .background(.blue)
+                    .cornerRadius(8)
+                }
+                .background(SharingViewController(isPresenting: $isShare) {
+                    let activityVC = UIActivityViewController(activityItems: [shareText], applicationActivities: nil)
+                    
+                    // For iPad
+                    // if UIDevice.current.userInterfaceIdiom == .pad {
+                    //     av.popoverPresentationController?.sourceView = UIView()
+                    // }
+                    
+                    activityVC.completionWithItemsHandler = { _, _, _, _ in
+                        isShare = false
+                    }
+                    return activityVC
+                })
+            }
+        }
+    }
+    
+    struct SharingViewController: UIViewControllerRepresentable {
+        @Binding var isPresenting: Bool
+        var content: () -> UIViewController
         
-        rootVC.present(activityVC, animated: true)
+        func makeUIViewController(context: Context) -> UIViewController {
+            UIViewController()
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+            if isPresenting {
+                uiViewController.present(content(), animated: true)
+            }
+        }
     }
 }
