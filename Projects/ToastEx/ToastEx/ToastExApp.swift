5407788a5e772293527a293056dbbe597d6f4a4b
KEEN <homurahomus@gmail.com>
2022-11-16 15:00:16 +0900

    add: ToastEx

---
+++ Projects/ToastEx/ToastEx/ToastExApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToastExApp.swift
+//  ToastEx
+//
+//  Created by KeenKim on 2022/11/16.
+//
+
+import SwiftUI
+
+@main
+struct ToastExApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
