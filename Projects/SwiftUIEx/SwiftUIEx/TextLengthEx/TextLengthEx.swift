66380254d57f16595d71ec4234e7186c32bc61ee
KEEN <homurahomus@gmail.com>
2022-11-27 00:41:44 +0900

    add more ex

--- Projects/SwiftUIEx/SwiftUIEx/TextLengthEx/TextLengthEx.swift
+++ Projects/SwiftUIEx/SwiftUIEx/TextLengthEx/TextLengthEx.swift
@@ -11,32 +11,35 @@ struct TextLengthEx: View {
     @State var isShow: Bool = false
     
     var body: some View {
-        VStack {
-            VStack(alignment: .leading) {
-                Text("You can deposit from an external ETH wallet to this ETH deposit address or QR code.")
-                    .font(.system(size: 14))
-                    .background(.red)
-                Text("You should deposit ETH using the Ethereum network.")
-                    .font(.system(size: 14, weight: .bold))
-                    .background(.orange)
-                Text("You should deposit ETH using the Ethereum networ")
-                    .font(.system(size: 14, weight: .bold))
-                    .background(.green)
-                Text("You should deposit ETH using the Ethereum network.")
-                    .fixedSize()
-                    .font(.system(size: 14, weight: .bold))
-                    .background(.blue)
-                Text("You should deposit ETH using the Ethereum network. test longer text")
-                    .font(.system(size: 14, weight: .bold))
-                    .minimumScaleFactor(0.01)
-                    .lineLimit(2)
-                    .frame(width: UIScreen.main.bounds.width - 16, alignment: .leading)
-                    .background(.purple)
+        ScrollView(.vertical) {
+            VStack {
+                VStack(alignment: .leading) {
+                    Text("You can deposit from an external ETH wallet to this ETH deposit address or QR code.")
+                        .font(.system(size: 14))
+                        .background(.red)
+                    Text("You should deposit ETH using the Ethereum network.")
+                        .font(.system(size: 14, weight: .bold))
+                        .background(.orange)
+                    Text("You should deposit ETH using the Ethereum networ")
+                        .font(.system(size: 14, weight: .bold))
+                        .background(.green)
+                    Text("You should deposit ETH using the Ethereum network.")
+                        .font(.system(size: 14, weight: .bold))
+                        .minimumScaleFactor(0.01)
+                        .lineLimit(2)
+                        .background(.blue)
+                    Text("You should deposit ETH using the Ethereum network. test longer text")
+                        .font(.system(size: 14, weight: .bold))
+                        .minimumScaleFactor(0.01)
+                        .lineLimit(2)
+                        .frame(width: UIScreen.main.bounds.width - 16, alignment: .leading)
+                        .background(.purple)
+                }
+                .background(.white)
             }
-            .background(.white)
+            .background(.gray)
+            .padding(16)
         }
-        .background(.gray)
-        .padding(16)
     }
 }
 
