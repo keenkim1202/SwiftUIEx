7ed85a71dc362015677510d14d774ec7758bd295
KEEN <homurahomus@gmail.com>
2023-02-24 13:28:52 +0900

    add: navigatio ex

---
+++ Projects/NavigationEx/NavigationEx/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NavigationEx
+//
+//  Created by KeenKim on 2023/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Projects/NavigationEx/NavigationEx/NavigationExApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationExApp.swift
+//  NavigationEx
+//
+//  Created by KeenKim on 2023/02/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationExApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
